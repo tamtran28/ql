7b4a83a0ff72e2949ab7e0bc961feb8ef1d1e63b
tam.tran <tranngocthanhtam28@gmail.com>
2023-07-10 09:17:39 +0700

    giaovien

---
+++ hocvien/Models/CLopTS.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace hocvien.Model
+{
+	public class CLopTS
+	{
+        public string Maloptuyensinh { get; set; }
+        public string Tenloptuyensinh { get; set; }
+        public string Trangthai { get; set; }
+        public string Nguoitao { get; set; }
+        public DateTime Ngaytao { get; set; }
+        public DateTime Ngaybatdau { get; set; }
+        public DateTime Ngayketthuc { get; set; }
+        public string Thuhoc { get; set; }
+        public string Macahoc { get; set; }
+        public string Makh { get; set; }
+        public string Mamh { get; set; }
+        public string Giohoc { get; set; }
+    }
+}
+
---
+++ hocvien/Models/CreateClassViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace hocvien.Model
+{
+	public class CreateClassViewModel
+	{
+        public string Makh { get; set; }
+        
+        public List<string> MamhList { get; set; }
+        public DateTime Ngaybatdau { get; set; }
+        public DateTime Ngayketthuc { get; set; }
+        public string Giohoc { get; set; }
+    }
+}
+
